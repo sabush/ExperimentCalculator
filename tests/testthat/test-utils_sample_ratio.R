d7e8ebe66e3232a7abbd1b7e7c23c688c30bb5ea
Stephen Bush <stephenbush1@gmail.com>
2024-07-25 16:09:08 +1000

    fix SRM test cases

--- tests/testthat/test-utils_sample_ratio.R
+++ tests/testthat/test-utils_sample_ratio.R
@@ -1,21 +1,18 @@
 test_that("sample ratio mismatch test is correct", {
   expect_equal(create_srm_text(data.frame(sample_size = c(1000, 1000),
                                           expected_proportion = c(0.5, 0.5)), 0.0001),
-               '<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 1)')
+               HTML('<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 1)'))
   expect_equal(create_srm_text(data.frame(sample_size = c(1000, 1200),
                                           expected_proportion = c(0.5, 0.5)), 0.0001),
-               '<font color = "#c30000"><b>There is a sample ratio mismatch error</b></font> (p-value = 0.00002) <br>Note that this means that experiment results are more likely to be untrustworthy, and the cause for the mismatch should be investigated.')
+               HTML('<font color = "#c30000"><b>There is a sample ratio mismatch error</b></font> (p-value = 0.00002) <br>Note that this means that experiment results are more likely to be untrustworthy, and the cause for the mismatch should be investigated.'))
   expect_equal(create_srm_text(data.frame(sample_size = c(1000, 1150),
                                           expected_proportion = c(0.5, 0.5)), 0.0001),
-               '<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 0.0012)')
+               HTML('<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 0.0012)'))
   expect_equal(create_srm_text(data.frame(sample_size = c(1000, 1150),
                                           expected_proportion = c(0.5, 0.5)), 0.01),
-               '<font color = "#c30000"><b>There is a sample ratio mismatch error</b></font> (p-value = 0.0012) <br>Note that this means that experiment results are more likely to be untrustworthy, and the cause for the mismatch should be investigated.')
+               HTML('<font color = "#c30000"><b>There is a sample ratio mismatch error</b></font> (p-value = 0.0012) <br>Note that this means that experiment results are more likely to be untrustworthy, and the cause for the mismatch should be investigated.'))
   expect_equal(create_srm_text(data.frame(sample_size = c(751, 249),
                                           expected_proportion = c(0.75, 0.25)), 0.0001),
-               '<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 0.94)')
+               HTML('<font color = "#1b680b"><b>There is no sample ratio mismatch error</b></font> (p-value = 0.94)'))
 })
 
-test_that("count table is a data.frame", {
-  expect_that(values$srm_DF, is_a("data.frame"))
-})

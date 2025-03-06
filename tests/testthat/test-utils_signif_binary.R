c9b5cd04d4bfc7d74d89d9b32d6c49552695d1d0
Stephen Bush <stephenbush1@gmail.com>
2025-03-06 20:00:23 +1100

    Fix more tests

--- tests/testthat/test-utils_signif_binary.R
+++ tests/testthat/test-utils_signif_binary.R
@@ -6,13 +6,13 @@ test_that("correct confidence intervals - absolute difference", {
   expect_equal(pair_abs_diff_summary_binary(42, 46, 1000, 1200, 0.05)$LCI,
                prop.test(c(46, 42), c(1200, 1000), correct = FALSE)$conf.int[1])
   expect_equal(pair_abs_diff_summary_binary(340, 460, 10000, 12000, 0.05)$UCI,
-               prop.test(c(460, 340), c(1200, 1000), correct = FALSE)$conf.int[2])
+               prop.test(c(460, 340), c(12000, 10000), correct = FALSE)$conf.int[2])
   expect_equal(pair_abs_diff_summary_binary(340, 460, 10000, 12000, 0.05)$LCI,
-               prop.test(c(460, 340), c(1200, 1000), correct = FALSE)$conf.int[1])
+               prop.test(c(460, 340), c(12000, 10000), correct = FALSE)$conf.int[1])
   expect_equal(pair_abs_diff_summary_binary(340, 460, 10000, 12000, 0.1)$UCI,
-               prop.test(c(460, 340), c(1200, 1000), conf.level = 0.9, correct = FALSE)$conf.int[2])
+               prop.test(c(460, 340), c(12000, 10000), conf.level = 0.9, correct = FALSE)$conf.int[2])
   expect_equal(pair_abs_diff_summary_binary(340, 460, 10000, 12000, 0.1)$LCI,
-               prop.test(c(460, 340), c(1200, 1000), conf.level = 0.9, correct = FALSE)$conf.int[1])
+               prop.test(c(460, 340), c(12000, 10000), conf.level = 0.9, correct = FALSE)$conf.int[1])
 })
 
 test_that("correct point estimate - absolute difference", {
--- tests/testthat/test-utils_signif_normal.R
+++ tests/testthat/test-utils_signif_normal.R
@@ -2,71 +2,56 @@ test_that("correct confidence intervals - absolute difference", {
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$UCI,
                5 - 4 +
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$LCI,
                5 - 4 -
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$UCI,
                5 - 2 +
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$LCI,
                5 - 2 -
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$UCI,
                5 - 4 +
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (1 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$LCI,
                5 - 4 -
                  qt(1 - 0.05/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (1 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (1 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$UCI,
                5 - 4 +
                  qt(1 - 0.1/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$LCI,
                5 - 4 -
                  qt(1 - 0.1/2, 500 + 500 - 2) *
-                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((500 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (500 + 500 - 2)) * sqrt(1 / 500 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$UCI,
                5 - 4 +
                  qt(1 - 0.05/2, 750 + 500 - 2) *
-                 sqrt(((750 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (750 + 500 - 2)) * sqrt(1 / 750 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((750 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (750 + 500 - 2)) * sqrt(1 / 750 + 1 / 500))
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$LCI,
                5 - 4 -
                  qt(1 - 0.05/2, 750 + 500 - 2) *
-                 sqrt(((750 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (750 + 500 - 2)) * sqrt(1 / 750 + 1 / 500),
-               tolerance = '3e')
+                 sqrt(((750 - 1) * (2 ** 2) + (500 - 1) * (2 ** 2)) / (750 + 500 - 2)) * sqrt(1 / 750 + 1 / 500))
 })
 
 test_that("correct point estimate - absolute difference", {
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$point_est,
-               5 - 4,
-               tolerance = '3e')
+               5 - 4)
   expect_equal(pair_abs_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$point_est,
-               5 - 2,
-               tolerance = '3e')
+               5 - 2)
   expect_equal(pair_abs_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$point_est,
-               5 - 4,
-               tolerance = '3e')
+               5 - 4)
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$point_est,
-               5 - 4,
-               tolerance = '3e')
+               5 - 4)
   expect_equal(pair_abs_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$point_est,
-               5 - 4,
-               tolerance = '3e')
+               5 - 4)
 })
 
 test_that("correct conclusion - absolute difference", {
@@ -85,71 +70,56 @@ test_that("correct confidence intervals - relative difference", {
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$UCI,
                (5 - 4) / 4 +
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$LCI,
                (5 - 4) / 4 -
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$UCI,
                (5 - 2) / 2 +
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(2 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (2 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(2 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (2 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$LCI,
                (5 - 2) / 2 -
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(2 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (2 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(2 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (2 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$UCI,
                (5 - 4) / 4 +
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((1 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((1 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$LCI,
                (5 - 4) / 4 -
                  qt(1 - 0.05/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((1 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((1 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$UCI,
                (5 - 4) / 4 +
                  qt(1 - 0.1/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$LCI,
                (5 - 4) / 4 -
                  qt(1 - 0.1/2, 1000 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(500)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$UCI,
                (5 - 4) / 4 +
                  qt(1 - 0.05/2, 1250 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(750)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(750)) ** 2)))
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$LCI,
                (5 - 4) / 4 -
                  qt(1 - 0.05/2, 1250 - 2) *
-                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(750)) ** 2)),
-               tolerance = '3e')
+                 sqrt(1/(4 ** 2) * ((2 /sqrt(500)) ** 2) + (5 ** 2) / (4 ** 4) * ((2 /sqrt(750)) ** 2)))
 })
 
 test_that("correct point estimate - relative difference", {
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.05)$point_est,
-               (5 - 4) / 4,
-               tolerance = '3e')
+               (5 - 4) / 4)
   expect_equal(pair_rel_diff_summary_normal(2, 5, 2, 2, 500, 500, 0.05)$point_est,
-               (5 - 2) / 2,
-               tolerance = '3e')
+               (5 - 2) / 2)
   expect_equal(pair_rel_diff_summary_normal(4, 5, 1, 2, 500, 500, 0.05)$point_est,
-               (5 - 4) / 4,
-               tolerance = '3e')
+               (5 - 4) / 4)
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 500, 500, 0.1)$point_est,
-               (5 - 4) / 4,
-               tolerance = '3e')
+               (5 - 4) / 4)
   expect_equal(pair_rel_diff_summary_normal(4, 5, 2, 2, 750, 500, 0.05)$point_est,
-               (5 - 4) / 4,
-               tolerance = '3e')
+               (5 - 4) / 4)
 })
 
 test_that("correct conclusion - relative difference", {
